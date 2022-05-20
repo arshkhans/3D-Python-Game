e827eab66b3a4fc22c2ec0ca1d1c30b4929227bd
Arsh Khan <khanarsh322@gmail.com>
2022-05-20 15:54:26 +0530

    Removed comments

--- Lv3.py
+++ Lv3.py
@@ -184,13 +184,9 @@ class Lv3():
             if not success:
                 print("Ignoring empty camera frame.")
             else:
-                # If loading a video, use 'break' instead of 'continue'.
-                # To improve performance, optionally mark the image as not writeable to
-                # pass by reference.
                 image.flags.writeable = False
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                 results = self.hands.process(image)
-                # Draw the hand annotations on the image.
                 image.flags.writeable = True
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
                 if results.multi_hand_landmarks:
